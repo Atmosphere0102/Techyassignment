1ae1fa857c601d007a01648d5515f70e5ce0c983
Atmosphere0102 <tgold051@gmail.com>
2025-07-18 21:51:46 +0100

    Hero video testimonial section

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const mobileMenu = document.getElementById("mobileMenu");
+    const closeMenu = document.getElementById("closeMenu");
+  
+    hamburger.addEventListener("click", () => {
+      mobileMenu.classList.add("show");
+    });
+  
+    closeMenu.addEventListener("click", () => {
+      mobileMenu.classList.remove("show");
+    });
+  });
+  
